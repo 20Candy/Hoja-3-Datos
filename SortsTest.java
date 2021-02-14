b49f3d1878d70de41f024a4c5f1d5fd5dff71aa1
Priscigs <39099667+Priscigs@users.noreply.github.com>
2021-02-14 16:34:56 -0600

    Add files via upload
    JUnit

--- SortsTest.java
+++ SortsTest.java
@@ -1,11 +1,42 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * @author Priscilla
+ */
+
 import java.util.Random;
 
 import org.junit.jupiter.api.Test;
 
 class SortsTest {
 
+	@Test
+	void BubbleSortTest() {
+		sorts s = new sorts();
+		Comparable[] actual = { 5, 1, 6, 2, 3, 4 };
+	    Comparable[] expected = { 1, 2, 3, 4, 5, 6 };
+	    s.BubbleSort(actual);
+	    assertArrayEquals(expected, actual);
+	}
+	
+	@Test
+	void GnomeSortTest() {
+		sorts s = new sorts();
+		Comparable[] actual = { 5, 1, 6, 2, 3, 4 };
+	    Comparable[] expected = { 1, 2, 3, 4, 5, 6 };
+	    s.GnomeSort(actual);
+	    assertArrayEquals(expected, actual);
+	}
+	
+	@Test
+	void QuickSortTest() {
+		sorts s = new sorts();
+		Comparable[] actual = { 5, 1, 6, 2, 3, 4 };
+	    Comparable[] expected = { 1, 2, 3, 4, 5, 6 };
+	    s.QuickSort(actual);
+	    assertArrayEquals(expected, actual);
+	}
+	
 	@Test
 	void MergeSortTest() {
 		sorts s = new sorts();
